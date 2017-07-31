0800b5f8d877108a508b046a8ae24640ee5a12a1
Markus Dobel <github@spam.dobel.click>
2017-07-31 12:55:01 +0200

    Fix Github Pages deployment

--- deploy-to-pages.sh
+++ deploy-to-pages.sh
@@ -8,7 +8,8 @@ echo "Uploading to Github Pages"
 sha=`git rev-parse --verify HEAD`
 
 cd $target
-git diff --quiet && git diff --staged --quiet || git commit -am "Deploy to Github Pages: ${sha}"
+git add .
+git diff --quiet && git diff --staged --quiet || git commit -m "Deploy to Github Pages: ${sha}"
 git push
 cd ..
 
