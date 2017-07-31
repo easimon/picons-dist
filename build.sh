63aa15e078b84abc7fbca19a845c186478a12f62
Markus Dobel <github@spam.dobel.click>
2017-07-31 14:41:53 +0200

    Add pngquant

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ for i in picons-source/build-source/logos/*${iconset}.svg; do
   svg=$i
   png=$target/$(basename "$i" ".svg").png
   echo "converting $svg"
-  rsvg-convert --keep-aspect-ratio --width=1000 --format=png --output=$png $svg
+  rsvg-convert --keep-aspect-ratio --width=1000 --format=png $svg | pngquant - > $png
 done
 
 echo "Renaming (removing iconset suffix)"
