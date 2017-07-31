3e294d8dae6745d7a3d1908459f1c4a4f660b288
Markus Dobel <github@spam.dobel.click>
2017-07-31 23:21:57 +0200

    First try in rescaling like the original

--- build.sh
+++ build.sh
@@ -6,39 +6,53 @@ pngquant="pngquant 256 -"
 
 . vars.sh
 
-echo "Quantize source PNGs"
-for i in picons-source/build-source/logos/*${iconset}.png; do
-  src=$i
-  dst=$(basename $i)
-  cat $i | $pngquant > $dst
-  echo -n '.'
-done
+rm -rf $build
+mkdir -p $build
 
-cp picons-source/build-source/logos/*${iconset}.png $target/
+echo "Copy source PNGs"
+cp picons-source/build-source/logos/*.png $build
 
-echo "Convert and quantize SVGs"
-for i in picons-source/build-source/logos/*${iconset}.svg; do
+echo "Convert SVGs"
+for i in picons-source/build-source/logos/*.svg; do
   svg=$i
-  png=$target/$(basename "$i" ".svg").png
-  rsvg-convert --keep-aspect-ratio --width=1000 --format=png $svg | $pngquant > $png
+  png=$build/$(basename "$i" ".svg").png
+  rsvg-convert --format=png $svg > $png
   echo -n '.'
 done
+echo
 
-echo "Rename (remove iconset suffix)"
-for i in $target/*.png; do
-  old=$i
-  new=$(echo $old | sed "s/\.${iconset}\.png/.png/")
-  mv $old $new
-  echo -n '.'
-done
+echo "Resize and compose"
+grep -v -e '^#' -e '^$' backgrounds.conf | while read lines; do
+  currentlogo=""
 
-echo "Link channel names"
-mkdir -p $target/snp
-for i in $(cat picons-source/build-source/snp-index); do
-  sid=$(echo $i | cut -f1 -d=)
-  chn=$(echo $i | cut -f2 -d=)
-  ln -s $target/$chn.png $target/snp/$sid.png
-  echo -n '.'
+  OLDIFS=$IFS
+  IFS=";"
+  line=($lines)
+  IFS=$OLDIFS
+
+  resolution=${line[0]}
+  resize=${line[1]}
+  type=${line[2]}
+  background=${line[3]}
+
+  dir=$target/$resolution/$type/$background
+  echo "Build $dir"
+  mkdir -p $dir
+
+  for file in $(cat picons-source/build-source/snp-index); do
+    sid=$(echo $file | cut -f1 -d=)
+    chn=$(echo $file | cut -f2 -d=)
+    dst=$dir/$sid.png
+    src=$build/$chn.$type.png
+    if [[ ! -f $src ]]; then
+      src=$build/$chn.default.png
+    fi
+    
+    bgfile=picons-source/build-source/backgrounds/$resolution/$background.png 
+    convert $bgfile \( $src -background none -bordercolor none -border 100 -trim -border 1% -resize $resize -gravity center -extent $resolution +repage \) -layers merge - 2> /dev/null | $pngquant 2> /dev/null > $dst
+    echo -n '.'
+  done
+  echo
 done
 
 #echo "Link service names - not yet functional"
--- vars.sh
+++ vars.sh
@@ -3,7 +3,8 @@
 set -euo pipefail
 
 iconset=default
+build=build
 target=target
 target_branch=gh-pages
 
-export iconset target target_branch
+export iconset target target_branch build
