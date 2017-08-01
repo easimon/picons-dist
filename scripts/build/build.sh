fca5f716a328651fa386bc574da72677b8bc31e0
Markus Dobel <github@spam.dobel.click>
2017-08-01 21:54:06 +0200

    simple build of sets, no compose

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -17,55 +17,80 @@ echo "Convert SVGs"
 for i in picons-source/build-source/logos/*.svg; do
   svg=$i
   png=$build/$(basename "$i" ".svg").png
-  rsvg-convert --format=png --keep-aspect-ratio --width=1000 $svg > $png
+  rsvg-convert --format=png --keep-aspect-ratio --width=1000 $svg | $pngquant > $png
   echo -n '.'
 done
 echo
 
-echo "Size of $build: $(du -sh $build | cut -f1)"
+echo "Size of $build folder: $(du -sh $build | cut -f1)"
 
-exit 0
+echo "Create aliases"
 
-echo "Resize and compose"
-grep -v -e '^#' -e '^$' backgrounds.conf | while read lines; do
-
-  OLDIFS=$IFS
-  IFS=";"
-  line=($lines)
-  IFS=$OLDIFS
-
-  resolution=${line[0]}
-  resize=${line[1]}
-  type=${line[2]}
-  background=${line[3]}
-  bgfile=picons-source/build-source/backgrounds/$resolution/$background.png
-  dir=$target/$resolution/$type/$background
-  echo "Resize and compose $resolution/$type/$background"
-  mkdir -p $dir
+for type in default light white; do
+  dir=$target/$type
   for file in $(cat picons-source/build-source/snp-index); do
     sid=$(echo $file | cut -f1 -d=)
     chn=$(echo $file | cut -f2 -d=)
     dst=$dir/$chn.png
     src=$build/$chn.$type.png
     if [[ ! -f $src ]]; then
-      src=$build/$chn.default.png
-    fi
+      ln -s ../default/$sid.png $dir/$sid.png
+    else
+      if [[ ! -f $dst ]]; then
+        cp $src $dst
+      fi
 
-    if [[ ! -f $dst ]]; then
-#      cp $src $dst
-#      convert $bgfile \( $src -background none -bordercolor none -border 100 -trim -border 1% -resize $resize -gravity center -extent $resolution +repage \) -layers merge - | $pngquant > $dst
-      convert $bgfile \( $src -background none -bordercolor none -border 100 -trim -border 1% -resize $resize -gravity center -extent $resolution +repage \) -layers merge - > $dst
-    fi
-
-    if [[ $chn.png != $sid.png ]]; then
-      dstlink=$dir/$sid.png
-      rm -f $dstlink
-      ln -s $chn.png $dstlink
+      if [[ $chn.png != $sid.png ]]; then
+        dstlink=$dir/$sid.png
+        rm -f $dstlink
+        ln -s $chn.png $dstlink
+      fi
     fi
     echo -n '.'
   done
-  echo
 done
+
+
+#echo "Resize and compose"
+#grep -v -e '^#' -e '^$' backgrounds.conf | while read lines; do
+#
+#  OLDIFS=$IFS
+#  IFS=";"
+#  line=($lines)
+#  IFS=$OLDIFS
+#
+#  resolution=${line[0]}
+#  resize=${line[1]}
+#  type=${line[2]}
+#  background=${line[3]}
+#  bgfile=picons-source/build-source/backgrounds/$resolution/$background.png
+#  dir=$target/$resolution/$type/$background
+#  echo "Resize and compose $resolution/$type/$background"
+#  mkdir -p $dir
+#  for file in $(cat picons-source/build-source/snp-index); do
+#    sid=$(echo $file | cut -f1 -d=)
+#    chn=$(echo $file | cut -f2 -d=)
+#    dst=$dir/$chn.png
+#    src=$build/$chn.$type.png
+#    if [[ ! -f $src ]]; then
+#      src=$build/$chn.default.png
+#    fi
+
+#    if [[ ! -f $dst ]]; then
+##       cp $src $dst
+##       convert $bgfile \( $src -background none -bordercolor none -border 100 -trim -border 1% -resize $resize -gravity center -extent $resolution +repage \) -layers merge - | $pngquant > $dst
+##      convert $bgfile \( $src -background none -bordercolor none -border 100 -trim -border 1% -resize $resize -gravity center -extent $resolution +repage \) -layers merge - > $dst
+#    fi
+
+#    if [[ $chn.png != $sid.png ]]; then
+#      dstlink=$dir/$sid.png
+#      rm -f $dstlink
+#      ln -s $chn.png $dstlink
+#    fi
+#    echo -n '.'
+#  done
+#  echo
+#done
 
 #echo "Link service names - not yet functional"
 #mkdir -p $target/srp
